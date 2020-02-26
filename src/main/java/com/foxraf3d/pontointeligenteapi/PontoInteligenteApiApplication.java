e41a3d521da2b0795ec0e3bb8240dca9b4cc4820
rafael <rafael@foxnote>
2020-02-25 23:44:44 -0300

    vai cassete

---
+++ src/main/java/com/foxraf3d/pontointeligenteapi/PontoInteligenteApiApplication.java
@@ -0,0 +1,13 @@
+package com.foxraf3d.pontointeligenteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligenteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteApiApplication.class, args);
+	}
+
+}
